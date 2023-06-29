dd74b74adfda57ead5512bc53e0fb7343f49347a
Roman Tsekhmeyster <uia80383@contiwan.com>
2023-06-29 18:26:38 +0200

    added info file

---
+++ info.py
@@ -0,0 +1,8 @@
+import ref
+from os import environ
+
+# Bot information
+SESSION = environ.get('SESSION', 'Media_search')
+API_ID = environ.get('API_ID', '15007648')
+API_HASH = environ.get('API_HASH', 'e05dbc6d779e558810fd4f58c7f18a8d')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6386746804:AAHibn8yFlVChYsTt9jt0IGi324EIxaBlao')
